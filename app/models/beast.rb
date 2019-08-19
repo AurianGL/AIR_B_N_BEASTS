23d9f80427ea35a80b1b4c7157dc06d11d156b6a
Alice Deveaux <alice.dvx@icloud.com>
2019-08-19 16:35:51 +0200

    Added Collection of TYPES to Beast Model

--- app/models/beast.rb
+++ app/models/beast.rb
@@ -1,8 +1,9 @@
 class Beast < ApplicationRecord
+  TYPES = ["Owl", "Elf", "Basilisk", "Hungarian Horntail", "Fawke", "Dementor", "Goblin", "Troll", "Cerberus"]
   mount_uploader :photo, PhotoUploader
-  validates :name, :description, :address, :dangerosity, :price, :type
+  validates :name, :description, :address, :dangerosity, :price, :type, presence: true
   belongs_to :user
-  #has_many :bookings
-  #has_many :reviews, through: :bookings
-  #has_many :users, through: :bookings
+  has_many :bookings
+  # has_many :reviews, through: :bookings
+  has_many :users, through: :bookings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :phone_number, presence: true
-  #has_many :beasts, :bookings
-  #has_many :reviews, through: :bookings
+  has_many :beasts, :bookings
+  # has_many :reviews, through: :bookings
 end
