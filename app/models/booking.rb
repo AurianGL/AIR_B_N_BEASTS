2c9f7f5eb830d05a4c17fca5d3c12fc04bcd74bd
Alice Deveaux <alice.dvx@icloud.com>
2019-08-19 16:27:24 +0200

    Added Booking Model with validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :beast
   belongs_to :user, through: beast
-  validates :start_date, :end_date, presence: true
+  validates :start_date, :end_date, :status, presence: true
 end
