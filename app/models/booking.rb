d8e542270e83acee144831995960bdcbb8aa1b71
Alice Deveaux <alice.dvx@icloud.com>
2019-08-19 17:13:58 +0200

    created SeedDB branch

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :beast
-  belongs_to :user, through: :beast
+  # belongs_to :user, through: :beast
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :status, presence: true
