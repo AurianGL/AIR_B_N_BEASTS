1fb0f5cc59805e58fbea71371fb150b0467f3fca
Aurian Guerard des Lauriers <aurian@hotmail.fr>
2019-08-19 17:39:35 +0200

    start user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  User.create(
+      name:    Faker::Company.name,
+      address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+      rating:  rand(0..5)
+    )
+end
+j
