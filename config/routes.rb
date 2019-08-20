aded949b781edc18ffc0aa9eebf5a2111bebe100
antoine schmitt <antoine.schmitt@gmail.com>
2019-08-20 10:07:52 +0200

    route index beast

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :beasts, only: [:create, :new, :update, :show] do
+    resources :beasts, only: [:create, :index, :new, :update, :show] do
     resources :bookings, only: [:create, :new, :update, :show]
   end
 end
