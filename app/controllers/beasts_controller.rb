ad02cecbd26768a2d70428ac7286f2935840f8cf
Alice Deveaux <alice.dvx@icloud.com>
2019-08-22 12:42:22 +0200

    added first name to sign up

--- app/controllers/beasts_controller.rb
+++ app/controllers/beasts_controller.rb
@@ -1,5 +1,5 @@
 class BeastsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  # skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @beasts = Beast.all
