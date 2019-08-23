ea07ab804b88271130032142584b7ee99b2ce91d
Alice Deveaux <alice.dvx@icloud.com>
2019-08-23 10:49:08 +0200

    starting again with clen slate

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -36,6 +36,12 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to dashboard_path
+  end
+
   private
 
   def booking_params
